95d7a1e42dc5ab639277aa63416ee91cc4c1655b
Tor Olav Kristensen <tor.olav.k@gmail.com>
2019-03-20 18:52:39 +0100

    added copyright/license information and fixed inconsistent use of quotation marks

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
+"""
+Copyright (c) 2019 Tor Olav Kristensen, http://subcube.com
+https://github.com/t-o-k/scikit-vectors
+Use of this source code is governed by a BSD-license that can be found in the LICENSE file.
+"""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "scikit-vectors",
-    version = "0.6.0",
-    author = "Tor Olav Kristensen",
-    author_email = "tor.olav.k@gmail.com",
-    description = "Functions to create n-dimensional vector classes",
+    name = 'scikit-vectors',
+    version = '0.6.0',
+    author = 'Tor Olav Kristensen',
+    author_email = 'tor.olav.k@gmail.com',
+    description = 'Functions to create n-dimensional vector classes',
     long_description = long_description,
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/t-o-k/scikit-vectors",
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/t-o-k/scikit-vectors',
     license = 'BSD',
     packages = setuptools.find_packages(),
     classifiers = \
@@ -20,9 +26,9 @@ setuptools.setup(
             'Intended Audience :: Developers',
             'Intended Audience :: Education',
             'Intended Audience :: Science/Research',
-            "License :: OSI Approved :: BSD License",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3",
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Software Development'
         ],
