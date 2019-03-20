ace950e7d10d58c1f24037a4853648c0a03746a4
Tor Olav Kristensen <tor.olav.k@gmail.com>
2019-03-20 19:00:04 +0100

    specified which minor versions of Python 3 that scikit-vectors can be used with

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ setuptools.setup(
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Scientific/Engineering :: Mathematics',
             'Topic :: Software Development'
         ],
